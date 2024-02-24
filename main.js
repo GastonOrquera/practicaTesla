8a8be6f59e873fd515bbe36552c1a3fd40703a31
GastonOrquera <gastonorquera29@gmail.com>
2024-02-24 06:42:30 -0300

    PruebaTesla

---
+++ main.js
@@ -0,0 +1,21 @@
+var hamburger = document.querySelector('.hamburger');
+
+var nav = document.querySelector('nav');
+
+var navLinks = document.querySelectorAll('nav a');
+
+var conteinerI = document.querySelector('.conteinerI');
+
+hamburger.addEventListener('click', function () {
+
+    nav.classList.toggle('active');
+    conteinerI.classList.toggle('active');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            nav.classList.remove('active');
+            conteinerI.classList.remove('active');
+        });
+    });
+
+});
