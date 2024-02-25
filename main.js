d41c68ac0a1783332c63b2f43de1fc90b46d0e1d
GastonOrquera <gastonorquera29@gmail.com>
2024-02-25 00:41:51 -0300

    practicaTesla

--- main.js
+++ main.js
@@ -9,11 +9,13 @@ var conteinerI = document.querySelector('.conteinerI');
 hamburger.addEventListener('click', function () {
 
     nav.classList.toggle('active');
+    hamburger.classList.toggle('active');
     conteinerI.classList.toggle('active');
 
     navLinks.forEach(function (link) {
         link.addEventListener('click', function () {
             nav.classList.remove('active');
+            hamburger.classList.remove('active');
             conteinerI.classList.remove('active');
         });
     });
